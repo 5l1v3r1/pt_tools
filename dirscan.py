facee790bed5cab298032d956aeac8396203bb1e
n00B <34676016+n00B-ToT@users.noreply.github.com>
2020-01-04 16:37:45 +0800

    Add files via upload

---
+++ dirscan.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python3
+
+import threading, argparse , requests , sys
+from queue import Queue
+#import pdb
+
+class DirScan:
+    def __init__(self , args):
+        self.urls = args.urls
+        self.thread_num = args.thread_num
+        self.wordlists = args.wordlists 
+
+    class n00b_DirScan(threading.Thread):
+        def __init__(self, queue , total):
+            threading.Thread.__init__(self)
+            self._queue = queue
+            self._total = total
+
+        def run(self):
+            while not self._queue.empty():
+                urls = self._queue.get()
+                try:
+                    resp = requests.head(urls)
+                    sys.stdout.write('success')
+                    if resp.status_code == 200 :
+                        sys.stdout.write('\r' + '[*]-----%s\t\t\n'%urls + "------------200")
+                    elif resp.status_code == 403 :
+                        sys.stdout.write('\r' + '[*]-----%s\t\t\n'%urls + "------------403")
+                    elif resp.status_code == 302 :
+                        sys.stdout.write('\r' + '[*]-----%s\t\t\n'%urls + "------------302")
+                except Exception as e:
+                    print(e)
+                    pass
+
+    def start(self):
+        queue = Queue()
+        f = open(self.wordlists,'r')
+        for i in f.readlines():
+            queue.put(self.urls + '/' + i.strip('\n'))
+        total = queue.qsize()
+        threads = []
+        #pdb.set_trace()
+        thread_count = int(self.thread_num)
+        
+        for i in range(thread_count):
+            threads.append(self.n00b_DirScan(queue,total))
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t',dest='thread_num',type=int,help="thread options",default=5)
+    parser.add_argument('-u',dest='urls',type=str,help="url options")
+    parser.add_argument('-w',dest='wordlists',type=str,help="wordlists options")
+    args = parser.parse_args()
+    if args.urls and args.wordlists:
+        dirscan = DirScan(args)
+        dirscan.start()
+        sys.exit(1)
+    else:
+        txt = '''
+        -w Please enter the WORDLIST file address
+        -t Please enter the THREAD number
+        -u Please enter the URL number
+        '''
+        print(txt)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
