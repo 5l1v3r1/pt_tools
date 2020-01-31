4ef661fa72d26e204ace2578ef77d8a5474ebad2
n00B <34676016+n00B-ToT@users.noreply.github.com>
2020-01-31 16:37:30 +0800

    Add files via upload

---
+++ moniter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import pexpect,smtplib,re  #, pdb
+from sys import argv
+from email.mime.text import MIMEText
+
+GREEN = "\033[0;32m"
+none = "\033[0m"
+
+
+def run(Port):
+        process = pexpect.spawn("nc",['-lvp',Port])
+#        pdb.set_trace()
+        process.expect_exact("connect")
+        string =  str(process.before)
+        ip = re.search(r'((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))',string,re.M|re.I)
+        print(ip.group())
+        em4il(ip)
+        process.interact()
+
+def em4il(ip):
+    msg_from = "xxxxx@xx.com"                   #sender
+    password = "xxxxx"                          #sender 's password
+    msg_to = "xxxx@xx.com"                      #receiver
+    subject = "U get A Reverse shell"
+    IPS =str(ip)
+    content = "U get a Reverse Shell  [ "+IPS+" ]  in Ur VPS \nU get a Reverse Shell  [ "+IPS+" ]  in Ur VPS \nU get a Reverse Shell  [ "+IPS+" ]  in Ur VPS \nU get a Reverse Shell  [ "+IPS+" ]  in Ur VPS \nU get a Reverse Shell  [ "+IPS+" ]  in Ur VPS \n"
+    msg = MIMEText(content)
+    msg['Subject'] = subject
+    msg['From'] = msg_from
+    msg['To'] = msg_to
+
+    try:
+        s = smtplib.SMTP('smtp.163.com',25)     #163's smtp
+        s.login(msg_from,password)
+        s.sendmail(msg_from , msg_to , msg.as_string())
+        print(GREEN + "[*]---------sucess" + none + '\n')
+
+    except smtplib.SMTPException as e :
+       print(GREEN+ "[!]---------failed" + format(e) + none + "\n")
+
+    finally:
+       s.quit()
+
+
+def main(): 
+    if len(argv)!=2 or argv[1] == '-h' or argv[1] == '--help':
+        print( GREEN +"[!]Usage: {} [PORT] ".format(argv[0]))
+        print(none+"\n")
+        exit()
+
+    Port = argv[1]
+    run(Port)
+
+
+if __name__ == '__main__':
+    main()
